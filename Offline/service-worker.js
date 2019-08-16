30d68391e4896a5086977250d0b1ef2d5afd232b
deltazintech <deltazintech@gmail.com>
2019-08-16 12:53:06 +0530

    Update service-worker.js

--- Offline/service-worker.js
+++ Offline/service-worker.js
@@ -1,39 +1,21 @@
-'use strict';
-
-var cacheVersion = 1;
-var currentCache = {
-  offline: 'offline-cache' + cacheVersion
-};
-const offlineUrl = 'offline-page.html';
-
-this.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(currentCache.offline).then(function(cache) {
+    caches.open('offline-cache-v1').then(function(cache) {
       return cache.addAll([
-          './img/offline.svg',
-          offlineUrl
-      ]);
+	  'index.html',
+	  'Calsci.js',
+	  'complete.ly.1.0.1.min.js',
+	  'style.css'
+	  ]);
     })
   );
 });
 
-this.addEventListener('fetch', event => {
-  // request.mode = navigate isn't supported in all browsers
-  // so include a check for Accept: text/html header.
-  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-        event.respondWith(
-          fetch(event.request.url).catch(error => {
-              // Return the offline page
-              return caches.match(offlineUrl);
-          })
-    );
-  }
-  else{
-        // Respond with everything else if we can
-        event.respondWith(caches.match(event.request)
-                        .then(function (response) {
-                        return response || fetch(event.request);
-                    })
-            );
-      }
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request)
+    })
+  );
 });
